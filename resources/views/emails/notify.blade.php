935a8c8c8d84c6beed953426811bd2589926fdfe
Reza Shadman <pcfeeler@gmail.com>
2015-02-07 02:36:44 +0330

    fix view typos

--- resources/views/emails/notify.blade.php
+++ resources/views/emails/notify.blade.php
@@ -4,7 +4,7 @@
     The Alexa Status of site <a href="{{ $site }}">{{ $site }}</a> has been initialized to be watched.
     <br>The country rank is currently : {{ $from }}
 @else
-    The Alexa Rank of site <a href="{{ $site }}">{{ $site }}</a> has been changed from "{{ $from }} to {{ $to }}.<br>
+    The Alexa Rank of site <a href="{{ $site }}">{{ $site }}</a> has been changed from "{{ $from }}" to "{{ $to }}".<br>
 @endif
 <br><br>
 -- Sent with <b><a href="http://github.com/bigsinoos/alexa-watcher">AlexaWatcher</a></b>
